4034c0f74d62a26a9a5d74c0b2ef18c967a283cc
sp <sourabhothers@gmail.com>
2020-10-19 19:28:22 +0530

    description field add

--- src/utils/getWeather.js
+++ src/utils/getWeather.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const request = require("request"),chalk=require('chalk')
 
 const cl = (d) => {
@@ -16,6 +17,7 @@ const filteredData = async (weatherData, enteredCity) => {
       city: weatherData.name,
       temperature: weatherData.main.temp,
       humidity: weatherData.main.humidity,
+      description: `${weatherData.weather[0].main}. ${weatherData.weather[0].description}`
     };
   } else if (weatherData.cod == 404 || weatherData.cod == 400) {
     return {
@@ -35,14 +37,14 @@ const getWeather = async (city, callBack) => {
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73642407cd5bdc62b6ea839f6cfd7ee4&units=metric`;
   let weatherData = { city: "Initial City", error: "Unknown" };
 
-  request.get({ url, json: true }, async (err, res) => {
+  request.get({ url, json: true }, async (err, {body}={}) => {
     if (err) {
       cl(`Error occured for : ${city}`);
       weatherData = { city: city, error: err };
       callBack(weatherData);
     } else {
       cl(`Request data received for : ${city}`);
-      weatherData = await filteredData(await res.body, city);
+      weatherData = await filteredData(await body, city);
       return callBack(weatherData || { error: "Unknown issue" });
     }
   });
